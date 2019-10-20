fb0b1a4ec5bbb72c8bb3757f95cc585aca07cc15
Robert Fernandez <robert.fernandez@mymona.edu.jm>
2019-10-19 22:09:08 -0500

    Hover Style

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,34 +5,40 @@ function grid() {
     
     //loop for each box
     for (let i=0;i<boxes.length;i++){
+        
         boxes[i].setAttribute("class","square");
-          boxes[i].setAttribute("onClick","test()");
+        boxes[i].setAttribute("onClick","place()");
+        boxes[i].setAttribute("onmouseover","hoverStyle()");
+        boxes[i].setAttribute("onmouseout","hoverOff()");
         
         
     }
 }
 
 window.onload=grid;
+
+//global variable
+var x_o =0;
+
+
 //box functions
-function test() {
-    /*var [] plays;*/
-    var x_o =0;
+function place() {
     
     //box assignment
     let boxDem = window.document.getElementById("board").children;
 
-    var box1 = boxDem[0], box2 = boxDem[1], box3 = boxDem[2],
+    /*var box1 = boxDem[0], box2 = boxDem[1], box3 = boxDem[2],
         box4 = boxDem[3], box5 = boxDem[4], box6 = boxDem[5],
-        box7 = boxDem[6], box8 = boxDem[7], box9 = boxDem[8];
+        box7 = boxDem[6], box8 = boxDem[7], box9 = boxDem[8];*/
 
-    /*box1.innerHTML = "lol";*/
+    
 
     //box checking
     for(var i=0; i<boxDem.length; i++) {
         let box = boxDem[i];
 
         box.onclick = function() {
-            //box.innerHTML = "X";
+            
             if(x_o%2 == 0 && box.innerHTML == ""){
                 box.innerHTML = "X";
                 box.classList.add("square","X");
@@ -49,3 +55,44 @@ function test() {
     
 }
 
+
+//hover function
+function hoverStyle() {
+    
+    //box assignment
+    let boxDem = window.document.getElementById("board").children;
+
+    //box checking
+    for(var i=0; i<boxDem.length; i++) {
+        let box = boxDem[i];
+
+        box.onmouseover = function() {
+           
+            box.classList.add("hover");
+           
+        };
+
+    }
+    
+}
+
+
+//hover-off function
+function hoverOff() {
+    
+    //box assignment
+    let boxDem = window.document.getElementById("board").children;
+
+    //box checking
+    for(var i=0; i<boxDem.length; i++) {
+        let box = boxDem[i];
+
+        box.onmouseout = function() {
+           
+            box.classList.remove("hover");
+           
+        };
+
+    }
+    
+}

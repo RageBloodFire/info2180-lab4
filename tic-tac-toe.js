c3299e83d0b0e2ac3a97f8db28846bc10f1c7a53
Robert Fernandez <robert.fernandez@mymona.edu.jm>
2019-10-20 13:25:19 -0500

    Declare the Winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,9 +10,12 @@ function grid() {
         boxes[i].setAttribute("onClick","place()");
         boxes[i].setAttribute("onmouseover","hoverStyle()");
         boxes[i].setAttribute("onmouseout","hoverOff()");
+        //boxes[i].setAttribute("onClick","winner()");
         
         
     }
+    
+//    winner();
 }
 
 window.onload=grid;
@@ -26,11 +29,6 @@ function place() {
     
     //box assignment
     let boxDem = window.document.getElementById("board").children;
-
-    /*var box1 = boxDem[0], box2 = boxDem[1], box3 = boxDem[2],
-        box4 = boxDem[3], box5 = boxDem[4], box6 = boxDem[5],
-        box7 = boxDem[6], box8 = boxDem[7], box9 = boxDem[8];*/
-
     
 
     //box checking
@@ -42,11 +40,13 @@ function place() {
             if(x_o%2 == 0 && box.innerHTML == ""){
                 box.innerHTML = "X";
                 box.classList.add("square","X");
+                winner();
                 x_o +=1;
             }
             else if(x_o%2 == 1 && box.innerHTML == ""){
                 box.innerHTML = "O";;
                 box.classList.add("square","O");
+                winner();
                 x_o +=1;
             }
         };
@@ -93,6 +93,86 @@ function hoverOff() {
            
         };
 
+    }
+    
+}
+
+
+//which winner fucntion- determines whether X or O won
+function whichWin() {
+    
+    //div assignment
+    let stat = document.getElementById("status");
+    
+    //box assignment
+    let boxDem = window.document.getElementById("board").children;
+    
+    for(var i=0;i<boxDem.length;i++) {
+        
+        boxDem[i].removeAttribute("onClick");
+        
+    }
+    
+ 
+    //checking who won
+    if(x_o%2==0) {
+        
+        stat.innerHTML = " ";
+        stat.innerHTML = "Congratulations! X is the Winner!";
+        stat.classList.add("you-won");
+    }
+    else if(x_o%2==1) {
+        
+        stat.innerHTML = " ";
+        stat.innerHTML = "Congratulations! O is the Winner!";
+        stat.classList.add("you-won");
+    }
+    
+}
+
+
+//winner function
+function winner() {
+    
+    //box assignment
+    let boxDem = window.document.getElementById("board").children;
+    
+    var box1 = boxDem[0], box2 = boxDem[1], box3 = boxDem[2],
+        box4 = boxDem[3], box5 = boxDem[4], box6 = boxDem[5],
+        box7 = boxDem[6], box8 = boxDem[7], box9 = boxDem[8];
+    
+    //check who won
+    if(box1.innerHTML !== "" && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML) {
+        
+        whichWin();
+    }
+    else if(box1.innerHTML !== "" && box1.innerHTML === box4.innerHTML && box1.innerHTML === box7.innerHTML) {
+        
+        whichWin();
+    }
+    else if(box1.innerHTML !== "" && box1.innerHTML === box5.innerHTML && box1.innerHTML === box9.innerHTML) {
+        
+        whichWin();
+    }
+    else if(box2.innerHTML !== "" && box2.innerHTML === box5.innerHTML && box2.innerHTML === box8.innerHTML) {
+        
+        whichWin();
+    }
+    else if(box3.innerHTML !== "" && box3.innerHTML === box6.innerHTML && box3.innerHTML === box9.innerHTML) {
+        
+        whichWin();
+    }
+    else if(box3.innerHTML !== "" && box3.innerHTML === box5.innerHTML && box3.innerHTML === box7.innerHTML) {
+        
+        whichWin();
+    }
+    else if(box4.innerHTML !== "" && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML) {
+        
+        whichWin();
+    }
+    else if(box7.innerHTML !== "" && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML) {
+        
+        whichWin();
     }
     
 }
